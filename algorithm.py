8b438a2e6bf99ad321aa5f9ebca426427f1f3c91
kayhser <cesarhrccontato@gmail.com>
2024-05-26 11:59:16 -0300

    Starting

--- algorithm.py
+++ algorithm.py
@@ -1,2 +1,11 @@
 import numpy as np
 from scipy.stats import pearsonr
+
+class PersonR():
+    Results = {}
+    def __init__(self, entry):
+        self.entry = entry
+    
+    def printResults(self):
+        for result in self.Results:
+            print(f"Para o usuario")
