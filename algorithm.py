ad5dc0853da2b8485c03818f9118be3bca07f9e8
kayhser <cesarhrccontato@gmail.com>
2024-05-26 22:18:15 -0300

    doing

--- algorithm.py
+++ algorithm.py
@@ -1,11 +1,19 @@
 import numpy as np
-from scipy.stats import pearsonr
+from scipy.stats import pearsonr as cor
 
 class PersonR():
+    Fields = ["Estabelecer um ambiente de concentracao",
+              "Aprofundamento por material teorico",
+              "Estudo por meio de aulas complementares",
+              "Estudo por discencia",
+              "Curiosidade individual",
+              "Estabelecimento de rotina",
+              "Longos Periodos de Estudo",
+              "Resolucao de problemas externos",
+              "Tempo de antecedenta para assimilacao"]
     Results = {}
     def __init__(self, entry):
         self.entry = entry
     
-    def printResults(self):
-        for result in self.Results:
-            print(f"Para o usuario")
+    def similarityVector():
+
